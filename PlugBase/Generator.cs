0e6989e5c9cfe8f4040adaa37e85bc33466e9a59
mvitiav <42879338+mvitiav@users.noreply.github.com>
2020-10-28 14:39:54 +0300

    second part

---
+++ PlugBase/Generator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlugBase
+{
+    public interface Generator
+    {
+        object generate(Type t);
+    }
+}
---
+++ SeparatePlugin1/PluginBody.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+
+using PlugBase;
+
+namespace SeparatePlugin1
+{
+    class PluginBody : Generator
+ 
+    {
+
+        private static RNGCryptoServiceProvider rngProvider = new RNGCryptoServiceProvider();
+        private static Random random = new Random(DateTime.Now.Millisecond);
+        private static byte[] someBytes;
+        public static BigInteger GetRandomBigInteger(int nBits, bool onlyPositive = true)
+        {
+            someBytes = new byte[nBits / 8];
+            rngProvider.GetBytes(someBytes);
+
+            BigInteger bg = new BigInteger(someBytes);
+            if (onlyPositive && bg.Sign == -1) bg = bg * -1;
+
+            return bg;
+        }
+        Random rnd = new Random();
+        public bool IsPrime(int n)
+        {
+            for (int i = 3; i * i <= n; i += 2)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public object generate(Type t)
+        {
+
+            ////if (t.Equals(typeof(int)))
+            ////{
+            ////    return (object)FindPrime(0,int.MaxValue,5);
+            ////}
+
+            //int i = 131;
+            ////int i = rnd.Next();
+            ////while (!IsPrime(i)) {
+            ////    i++;
+            ////}
+
+
+            if (t.Equals(typeof(BigInteger)))
+            {
+                return (object)GetRandomBigInteger(random.Next(10)*8);
+            }
+            else {
+                return (0);
+            }
+
+            //return (object)i;
+
+
+
+        }
+
+
+    }
+}
